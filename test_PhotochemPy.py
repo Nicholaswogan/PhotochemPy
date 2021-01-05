fbe583656a875aa3ef27eb33e64c1d288d2f088b
Nicholaswogan <wogan@uw.edu>
2021-01-05 13:08:02 -0800

    travis

--- test_PhotochemPy.py
+++ test_PhotochemPy.py
@@ -1,9 +1,4 @@
-import numpy as np
-from matplotlib import pyplot as plt
 from PhotochemPy import PhotochemPy
-import time
-import sys
-
 
 pc = PhotochemPy('input/templates/Archean+Haze/species.dat', \
               'input/templates/Archean+Haze/reactions.rx', \
@@ -12,25 +7,4 @@ pc = PhotochemPy('input/templates/Archean+Haze/species.dat', \
               'input/templates/Archean+Haze/atmosphere.txt', \
               'input/templates/Archean+Haze/Sun_2.7Ga.txt')
 
-
-converged = pc.integrate(nsteps=500)
-
-
-plot = False
-
-
-# print(pc.out_dict()['CO2'])
-if plot:
-    out = pc.out_dict()
-    plt.rcParams.update({'font.size': 15})
-    fig,ax = plt.subplots(1,1,figsize=[9,5])
-    specs = ['CH4','CO','O2','H2']
-    for sp in specs:
-        ax.plot(out[sp],out['alt'],label=sp)
-
-    ax.set_xscale('log')
-    ax.legend()
-    ax.set_ylabel('Altitude (km)')
-    ax.set_xlabel('Mixing Ratio')
-    # plt.savefig("Comparison.jpg",dpi = 100,bbox_inches='tight')
-    plt.show()
+converged = pc.integrate()
