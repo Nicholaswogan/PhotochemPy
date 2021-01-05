b9448f1586045000c1da7b74087ff8844c2810af
Nicholaswogan <wogan@uw.edu>
2021-01-05 13:28:17 -0800

    travis

--- test_PhotochemPy.py
+++ test_PhotochemPy.py
@@ -2,7 +2,7 @@ from PhotochemPy import PhotochemPy
 
 pc = PhotochemPy('input/templates/Archean+Haze/species.dat', \
               'input/templates/Archean+Haze/reactions.rx', \
-              'input/templates/Archean+Haze/planet.dat', \
+              'input/templates/Archean+Haze/PLANET.dat', \
               'input/templates/Archean+Haze/input_photchem.dat', \
               'input/templates/Archean+Haze/atmosphere.txt', \
               'input/templates/Archean+Haze/Sun_2.7Ga.txt')
