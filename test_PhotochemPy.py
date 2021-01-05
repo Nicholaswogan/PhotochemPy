927e2f3770e58c7ea454475a77e43589350346ef
Nicholaswogan <wogan@uw.edu>
2021-01-05 13:30:45 -0800

    travis

--- test_PhotochemPy.py
+++ test_PhotochemPy.py
@@ -1,10 +1,14 @@
 from PhotochemPy import PhotochemPy
 
-pc = PhotochemPy('input/templates/Archean+Haze/species.dat', \
-              'input/templates/Archean+Haze/reactions.rx', \
-              'input/templates/Archean+Haze/PLANET.dat', \
-              'input/templates/Archean+Haze/input_photchem.dat', \
-              'input/templates/Archean+Haze/atmosphere.txt', \
-              'input/templates/Archean+Haze/Sun_2.7Ga.txt')
+pc = PhotochemPy('input/templates/Archean+haze/species.dat', \
+              'input/templates/Archean+haze/reactions.rx', \
+              'input/templates/Archean+haze/PLANET.dat', \
+              'input/templates/Archean+haze/input_photchem.dat', \
+              'input/templates/Archean+haze/atmosphere.txt', \
+              'input/templates/Archean+haze/Sun_2.7Ga.txt')
+
+
+
+
 
 converged = pc.integrate()
