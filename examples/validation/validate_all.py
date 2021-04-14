8ee209af9d7d7a30ff15393c1a4b79f794b32c53
Nicholaswogan <wogan@uw.edu>
2021-04-13 17:31:03 -0700

    changed all templates so atmosphere is at equilibrium, and no photolyzing SL species. This is for CVODE. Also removed validation (no longer applicable) and adjusted the valgrind tests

--- examples/validation/validate_all.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-subprocess.run('python Hadean+HCN.py'.split())
-subprocess.run('python Archean+haze.py'.split())
-subprocess.run('python ArcheanSORG+haze.py'.split())
-subprocess.run('python ModernEarth.py'.split())
