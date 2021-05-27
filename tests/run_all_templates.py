bac8578f34b6f0e0995a9921d2b86ba40d05bf3d
Nicholaswogan <wogan@uw.edu>
2021-05-27 09:17:43 -0700

    updated intial atmospheres. fixed tests.

--- tests/run_all_templates.py
+++ tests/run_all_templates.py
@@ -4,12 +4,11 @@ from PhotochemPy import PhotochemPy
 ###############################
 ##### ModernEarth #####
 ###############################
-template = '../../input/templates/ModernEarth'
+template = '../input/templates/ModernEarth'
 star = 'Sun_now.txt'
 pc = PhotochemPy(template+'/species.dat', \
                  template+'/reactions.rx', \
-                 template+'/planet.dat', \
-                 template+'/input_photchem.dat', \
+                 template+'/settings.yaml', \
                  template+'/atmosphere.txt', \
                  template+'/'+star)
 pc.integrate(method='Backward_Euler')
@@ -18,12 +17,11 @@ pc.integrate(method='CVODE_BDF')
 ###############################
 ##### Archean2Proterozoic #####
 ###############################
-template = '../../input/templates/Archean2Proterozoic'
+template = '../input/templates/Archean2Proterozoic'
 star = 'Sun_2.7Ga.txt'
 pc = PhotochemPy(template+'/species.dat', \
                  template+'/reactions.rx', \
-                 template+'/planet.dat', \
-                 template+'/input_photchem.dat', \
+                 template+'/settings.yaml', \
                  template+'/atmosphere.txt', \
                  template+'/'+star)
 pc.integrate(method='Backward_Euler')
@@ -32,12 +30,11 @@ pc.integrate(method='CVODE_BDF')
 ########################
 ##### Archean+Haze #####
 ########################
-template = '../../input/templates/Archean+haze'
+template = '../input/templates/Archean+haze'
 star = 'Sun_2.7Ga.txt'
 pc = PhotochemPy(template+'/species.dat', \
                  template+'/reactions.rx', \
-                 template+'/planet.dat', \
-                 template+'/input_photchem.dat', \
+                 template+'/settings.yaml', \
                  template+'/atmosphere.txt', \
                  template+'/'+star)
 pc.integrate(method='Backward_Euler')
@@ -46,26 +43,11 @@ pc.integrate(method='CVODE_BDF')
 ########################
 ###### Hadean+HCN ######
 ########################
-template = '../../input/templates/Hadean+HCN'
+template = '../input/templates/Hadean+HCN'
 star = 'Sun_4.0Ga.txt'
 pc = PhotochemPy(template+'/species.dat', \
                  template+'/reactions.rx', \
-                 template+'/planet.dat', \
-                 template+'/input_photchem.dat', \
-                 template+'/atmosphere.txt', \
-                 template+'/'+star)
-pc.integrate(method='Backward_Euler')
-pc.integrate(method='CVODE_BDF')
-
-############################
-##### Hadean+HCN_LLCO2 #####
-############################
-template = '../../input/templates/Hadean+HCN_LLCO2'
-star = 'Sun_4.0Ga.txt'
-pc = PhotochemPy(template+'/species.dat', \
-                 template+'/reactions.rx', \
-                 template+'/planet.dat', \
-                 template+'/input_photchem.dat', \
+                 template+'/settings.yaml', \
                  template+'/atmosphere.txt', \
                  template+'/'+star)
 pc.integrate(method='Backward_Euler')
