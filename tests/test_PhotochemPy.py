3100d48282951edfb23c10911ab412e12aff5074
Nicholaswogan <wogan@uw.edu>
2021-07-27 23:31:03 -0700

    adjusted travis

---
+++ tests/test_PhotochemPy.py
@@ -0,0 +1,13 @@
+from PhotochemPy import PhotochemPy
+
+template = 'Archean2Proterozoic'
+sun = 'Sun_2.7Ga.txt'
+
+pc = PhotochemPy('input/templates/'+template+'/species.dat', \
+                 'input/templates/'+template+'/reactions.rx', \
+                 'input/templates/'+template+'/settings.yaml', \
+                 'input/templates/'+template+'/atmosphere.txt', \
+                 'input/templates/'+template+'/'+sun)
+
+converged = pc.integrate(method='Backward_Euler')
+converged = pc.integrate(method='CVODE_BDF')
